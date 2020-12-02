e4f3467dda1614191cd58c0a40b11481eadc16c1
Angelika Ehrlich <angelikaehrlich@gmx.net>
2020-12-02 15:36:54 +0100

    add playercontroller movement
    simple character movement (left, right)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = .1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0);
+
+        transform.position += moveDirection * speed;
+    }
+}
