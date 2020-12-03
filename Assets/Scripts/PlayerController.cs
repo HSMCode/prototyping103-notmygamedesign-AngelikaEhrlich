d6d01ffc6501f0dff48fb430f0fdfb32bd599ce7
Angelika Ehrlich <angelikaehrlich@gmx.net>
2020-12-03 18:51:20 +0100

    add GameOverPanel

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = .1f;
-    public int score;       //players score
-    public int lives;       //this is the number of lives the player has
+    public int score;                   //players score
+    public int lives;                   //this is the number of lives the player has
+    public Text livesText;              //link to the LivesText on  the screen
+    public Text scoreText;              //link to the ScoreText on the screen
+    public GameObject gameOverPanel;    //link to the gameOverPanel in the scene
 
     private void Start()
     {
         score = 0;
         lives = 5;
+        livesText.text = "Lives: " + lives.ToString();
+        scoreText.text = "Score: " + score.ToString();
+
+        //turn off gameOverPanel
+        gameOverPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -43,6 +53,8 @@ public class PlayerController : MonoBehaviour
     {
         //add points to the score
         score += 1;
+        //update the score text on the screen
+        scoreText.text = "Score: " + score.ToString();
     }
 
     // ScorePoints will be called from the FallingCoalScript when a collision occures between the player and the coal object
@@ -50,6 +62,8 @@ public class PlayerController : MonoBehaviour
     {
         //subtract points to the score
         score -= 1;
+        //update the score text on the screen
+        scoreText.text = "Score: " + score.ToString();
     }
 
     // LoseLife will be called from the FallingCoalScript when a collision occures between the player and the coal object
@@ -58,17 +72,36 @@ public class PlayerController : MonoBehaviour
         lives--;    //subtract 1 from lives
 
         //check to see if we have are out of lives
-        if(lives < 1)
+        if (lives < 1)
         {
             //out of lives
             GameOver();
+            lives = 0;
         }
-    }
 
+        //update the lives text on the screen
+        livesText.text = "Lives: " + lives.ToString();
+    }
     void GameOver()
     {
         //Make player disappear and be disabled
         gameObject.SetActive(false);
+
+        //turn on gameOverPanel
+        gameOverPanel.SetActive(true);
+    }
+
+    //playAgainButton from the gameOverPanel
+    public void PlayAgain()
+    {
+        //restart the game
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit()
+    {
+        //close the window where the game is being played in 
+        Application.Quit();
     }
 
 }
